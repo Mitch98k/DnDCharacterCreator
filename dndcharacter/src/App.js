2f511c473b4f476b681095209f5ec83cf5679df2
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-10-26 20:18:12 -0600

    created a basic home page

---
+++ dndcharacter/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Dungeons and Dragons</h1>
+       <h2>5th Edition Character Creator</h2>
+      </header>
+      <body className="App-">
+        <button>
+          Create your character
+        </button>
+      </body>
+    </div>
+  );
+}
+
+export default App;
