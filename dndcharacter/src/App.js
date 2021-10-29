4fe3b1fbd62ada45962191d09ef33b34f700bf0d
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-10-28 23:45:00 -0600

    retrieved race names from api and displayed them evenly with flexbox

--- dndcharacter/src/App.js
+++ dndcharacter/src/App.js
@@ -7,12 +7,12 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Route path='/races'>
+        <RaceSelection/>
+      </Route>
       <Route exact path='/'>
         <Homepage/>
       </Route>
-      <route path='/races'>
-        <RaceSelection/>
-      </route>
     </div>
   );
 }
--- dndcharacter/src/components/RaceSelection.js
+++ dndcharacter/src/components/RaceSelection.js
@@ -1,13 +1,30 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import '../css/Common.css';
+import '../css/RaceSelection.css';
 
 function RaceSelection() {
+    const [races, setRaces] = useState([]);
     const [race, setRace] = useState("");
 
+    useEffect(() => {
+        axios.get('https://www.dnd5eapi.co/api/races')
+        .then(res => {
+            setRaces(res.data.results);
+        })
+        .catch(err => console.log(err));
+    }, []);
+
     return (
-        <div>
+        <div className="race">
+          <header className="race-header">
             <h1>select your race</h1>
+          </header>
+          <body className = "race-body">
+            {races.map(r => (
+                <h2 key={r.index} className="races">{r.name}</h2>
+            ))}
+          </body>
         </div>
     );
 }
