355a996d8a5844f41c1653fcf37d476f08ca5f0a
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2021-11-10 17:58:01 -0700

    caused Race page to render information according to which race is chosen to see more info

--- dndcharacter/src/components/Race.js
+++ dndcharacter/src/components/Race.js
@@ -4,15 +4,51 @@ import axios from 'axios';
 
 function Race() {
     const { id } = useParams();
+
+    const [name, setName] = useState('');
+    const [abilities, setAbilities] = useState([]);
+    const [age, setAge] = useState('');
+    const [alignment, setAlignment] = useState('');
+    const [languages, setLanguages] = useState('');
+    const [size, setSize] = useState('');
+    const [speed, setSpeed] = useState('');
+    const [proficiencies, setProficiencies] = useState([]);
+    const [subraces, setSubraces] = useState([]);
+    const [traits, setTraits] = useState([]);
+
     useEffect(() => {
         axios.get(`https://www.dnd5eapi.co/api/races/${id}`)
         .then(res => {
-            console.log(res.data)
+            console.log(res.data);
+
+            setName(res.data.name);
+            setAbilities(res.data.ability_bonuses);
+            setAge(res.data.age);
+            setAlignment(res.data.alignment);
+            setLanguages(res.data.language_desc);
+            setSize(res.data.size_description);
+            setSpeed(res.data.speed);
+            setProficiencies(res.data.starting_proficiencies);
+            setSubraces(res.data.subraces);
+            setTraits(res.data.traits);
         })
         .catch(err => console.log(err));
     }, id);
     return (
-        <p>this race is {id}</p>
+        <div>
+            <h1>{name}</h1>
+            {abilities.map(a => (
+                <h4 key={a.ability_score.index}>{a.ability_score.name}: {a.bonus}</h4>
+            ))}
+            <p>age: {age}</p>
+            <p>alignment: {alignment}</p>
+            <p>languages: {languages}</p>
+            <p>size: {size}</p>
+            <p>your speed is {speed}</p>
+            {/* {proficiencies.map(p => (
+                <h3></h3>
+            ))} */}
+        </div>
     )
 }
 export default Race;
