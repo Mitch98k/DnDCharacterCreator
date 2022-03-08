690eca6e4b6e89bdd95cca4f11a22df81ce44aac
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2022-03-07 21:26:55 -0700

    rendering all alignments on alignment selection component

--- dndcharacter/src/components/AlignmentSelection.js
+++ dndcharacter/src/components/AlignmentSelection.js
@@ -6,8 +6,41 @@ import '../css/Common.css';
 function AlignmentSelection() {
     const { push } = useHistory();
 
+    const [alignments, setAlignments] = useState([]);
+    const [alignment, setAlignment] = useState();
+
+    useEffect(() => {
+        axios.get('https://www.dnd5eapi.co/api/alignments')
+        .then(res => {
+            console.log(res);
+            setAlignments(res.data.results);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    });
+
     return (
-        <div>Alignments</div>
+        <div>
+            <header className="selection-header" style={{ backgroundColor: 'grey'}}>
+                <button onClick={() => push('/')} className='home-btn'>Home</button>
+                <h1>select your alignment</h1>
+                <progress value='4' max='8'/>
+            </header>
+            <body className='selection-body'>
+                {alignments.map(all => (
+                    <div>
+                        <div className={alignment===all.name ? "selected" : "card"} key={all.name} onClick={() => setAlignment(all.name)}>
+                            <h2>{all.name}</h2>
+                        </div>
+                        <p onClick={() => push(`/alignments/${all.index}`)}>more info</p>
+                    </div>
+                ))}
+            </body>
+            <footer>
+                <button className='btn'>submit</button>
+            </footer>
+        </div>
     );
 };
 
